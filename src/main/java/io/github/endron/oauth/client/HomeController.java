b9ecda69bd092deec4ede4e087caf52d50d8c524
Tobias Kremer <tobias.kremer@comsysto.com>
2015-07-24 10:41:21 +0200

    inject OAuth2Authentication for more information
    it is the subclass of Principal used.

--- src/main/java/io/github/endron/oauth/client/HomeController.java
+++ src/main/java/io/github/endron/oauth/client/HomeController.java
@@ -1,10 +1,9 @@
 package io.github.endron.oauth.client;
 
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.security.Principal;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -14,7 +13,7 @@ public class HomeController {
 
     @RequestMapping(method = GET)
     @ResponseBody
-    public String get(final Principal principal) {
+    public String get(final OAuth2Authentication principal) {
         System.out.println(principal);
 
         return "Hello " + principal.getName();
