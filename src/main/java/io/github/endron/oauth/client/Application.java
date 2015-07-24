e9148b419beb64bef1c3b1a0538ac6c3777a34eb
Tobias Kremer <tobias.kremer@comsysto.com>
2015-07-24 10:26:05 +0200

    rename package

--- src/main/java/io/github/endron/oauth/client/Application.java
+++ src/main/java/io/github/endron/oauth/client/Application.java
@@ -1,4 +1,4 @@
-package io.github.endron.oauththing;
+package io.github.endron.oauth.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/io/github/endron/oauth/client/HomeController.java
+++ src/main/java/io/github/endron/oauth/client/HomeController.java
@@ -1,4 +1,4 @@
-package io.github.endron.oauththing;
+package io.github.endron.oauth.client;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
